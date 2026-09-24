4f6fdd6c72d713378c01d3481876f6e2e0a0e53a
Johan Stahrberg <johan_stahrberg@yahoo.se>
2026-09-24 16:28:44 +0200

    Add Library arrays and registration methods

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Member är en vanlig class eftersom medlemmen har tillstånd som kan förändras
 
 activeLoans ändras genom egna metoder i stället för en setter.
 På så sätt kan värdet ändras på ett mer kontrollerat sätt och inte sättas direkt till ett valfritt värde.
+
+### Library
+Library är en vanlig class eftersom den håller reda på bibliotekets föränderliga tillstånd.
+
+Böcker och medlemmar lagras i arrayer med fast storlek.
+
+En separat array, borrowedBy, används för att hålla reda på om en bok är utlånad och vilken medlem som har lånat den.
--- src/main/java/com/example/java26/Library.java
+++ src/main/java/com/example/java26/Library.java
@@ -2,5 +2,59 @@ package com.example.java26;
 
 public class Library {
 
+    private Book[] books = new Book[10];
+    private Member[] members = new Member[10];
+    private Member[] borrowedBy = new Member[10];
+
+    private int bookCount = 0;
+    private int memberCount = 0;
+
+    public void addBook(Book book) {
+        if (bookCount < books.length) {
+            books[bookCount] = book;
+            bookCount++;
+        } else {
+            System.out.println("Biblioteket har inte plats för fler böcker.");
+        }
+
+    }
+
+        public void registerMember(Member member) {
+            if (memberCount < members.length) {
+                members[memberCount] = member;
+                memberCount++;
+            } else {
+                System.out.println("Biblioteket har inte plats för fler medlemmar.");
+            }
+        }
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
